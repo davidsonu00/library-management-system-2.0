d38f51ba935e2b40a3cc410dcbcd937e782c7fc8
davidsonu00 <sonuguptaup9898@gmail.com>
2026-04-20 20:32:54 +0530

    3

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors(
-  // origin: [
-  //   "http://localhost:3000",
-  //   "https://libraryos-eight.vercel.app"
-  // ],
-  // credentials: true
-));
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://libraryos-eight.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
